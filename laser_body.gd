ca19cc5195824be1c400c40072190a87c0e2e2ee
Carlos Lopez <genetita@gmail.com>
2016-04-19 14:40:38 +0200

    Make laser die after some travel and turn around the screen

--- laser_body.gd
+++ laser_body.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 var velocity = 600
 var velocity_sign 
 var colors={ 1:Color(1,0,0,1), 2:Color(0,1,0,1), 3:Color(0,0,1,1), 4:Color(1, 1, 1, 1), 5:Color(1, 1, 0, 1), 6: Color(1, 0, 1, 1), 7: Color(0,1, 1, 1) }
+var total_motion=Vector2(0,0)
 
 func _ready():
 	set_fixed_process(true)
@@ -14,10 +15,21 @@ func _ready():
 func _fixed_process(delta):
 	var motion=Vector2(velocity, 0)*delta
 	move(motion)
+	total_motion+=motion
 	if is_colliding():
 		velocity=0
 		queue_free()
+	if total_motion.length() > 200:
+		print(total_motion)
+		queue_free()
+	var pos=get_pos()
+	var right_limit=get_viewport_rect().end.x
+	var left_limit=get_viewport_rect().pos.x
+	if(pos.x < left_limit):
+		set_pos(pos+Vector2(right_limit,0))
+	elif(pos.x > right_limit):
+		set_pos(pos-Vector2(right_limit, 0))
 
 func invert_direction():
 	velocity=-velocity
-	set_scale(Vector2(-1, 1))
+	get_node("laser_sprite").set_scale(Vector2(-1, 1))
