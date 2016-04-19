272f97ace7a0d57978b8515eb46ad731b1e4436b
Carlos Lopez <genetita@gmail.com>
2016-04-19 08:49:44 +0200

    Add random colors to laser

--- laser_body.gd
+++ laser_body.gd
@@ -3,10 +3,13 @@ extends KinematicBody2D
 
 var velocity = 600
 var velocity_sign 
+var colors={ 1:Color(1,0,0,1), 2:Color(0,1,0,1), 3:Color(0,0,1,1), 4:Color(1, 1, 1, 1), 5:Color(1, 1, 0, 1), 6: Color(1, 0, 1, 1), 7: Color(0,1, 1, 1) }
 
 func _ready():
 	set_fixed_process(true)
 	velocity_sign=velocity/abs(velocity)
+	var sprite=get_node("laser_sprite")
+	sprite.set_modulate(colors[randi()%7+1])
 
 func _fixed_process(delta):
 	var motion=Vector2(velocity, 0)*delta
