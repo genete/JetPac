ea5e33f4f13a18226a954c210878e392b1f9e2df
Carlos Lopez <genetita@gmail.com>
2016-05-11 08:15:31 +0200

    Move enemies to folder. Add header scene for player points
    Add global script to load scenes

---
+++ enemies/enemy1.gd
@@ -0,0 +1,62 @@
+##### ENEMY 7
+extends KinematicBody2D
+
+const HORIZONTAL_VELOCITY=40
+const VERTICAL_VELOCITY=15
+var velocity
+var colors={ 1:Color(1,0,0,1), 2:Color(0,1,0,1), 3:Color(0,0,1,1), 4:Color(1, 1, 1, 1), 5:Color(1, 1, 0, 1), 6: Color(1, 0, 1, 1), 7: Color(0,1, 1, 1) }
+var explosion= preload("res://explosion.tscn")
+
+signal enemy_died(p)
+
+func _ready():
+	randomize()
+	set_fixed_process(true)
+	var height=192-16-8-32
+	var width=256
+	var s=randi()%2
+	if s==0:
+		s=-1
+	velocity=Vector2(HORIZONTAL_VELOCITY*s, (0.2+randf())*VERTICAL_VELOCITY)
+	var sprite_width=get_node("Sprite").get_texture().get_width()
+	if s == -1:
+		set_pos(Vector2(-sprite_width, randf()*height+16))
+	else:
+		get_node("Sprite").set_scale(Vector2(-1,1))
+		get_node("Sprite").set_pos(Vector2(12,0))
+		set_pos(Vector2(width+sprite_width, randf()*height+16))
+	get_node("Sprite/anim").play("fly")
+	get_node("Sprite").set_modulate(colors[randi()%7+1])
+	add_user_signal("enemy_died")
+	connect("enemy_died", get_node("/root/World"), "_callback_enemy_died")
+
+func _fixed_process(delta):
+	var motion=velocity*delta
+	move(motion)
+	if is_colliding():
+		var collider=get_collider()
+		if collider.has_method("destroy"):
+			collider.destroy(true)
+		destroy(true)
+	var pos=get_pos()
+	var right_limit=256
+	var left_limit=0
+	if(pos.x < left_limit):
+		set_pos(pos+Vector2(right_limit,0))
+	elif(pos.x > right_limit):
+		set_pos(pos-Vector2(right_limit, 0))
+	if pos.x < left_limit-64 or pos.x >right_limit + 64 or pos.y < 0 or pos.y > 192:
+			destroy(false)
+
+		
+func destroy(var animate):
+	velocity=Vector2(0,0)
+	if animate:
+		var exp_instance=explosion.instance()
+		get_node("/root/World").add_child(exp_instance)
+		exp_instance.set_pos(get_pos()+Vector2(8, 8))
+		exp_instance.get_node("anim").play("explode")
+	queue_free()
+
+func get_points():
+	return 5
--- level.gd
+++ level.gd
@@ -8,14 +8,14 @@ const TOTAL_ENEMIES_TYPES=8
 const MAX_WAVES_PER_SHIP=3
 var enabled_enemies=true
 var enemies_scenes={
-1: preload("enemy1.tscn"),
-2: preload("enemy2.tscn"),
-3: preload("enemy3.tscn"),
-4: preload("enemy4.tscn"),
-5: preload("enemy5.tscn"),
-6: preload("enemy6.tscn"),
-7: preload("enemy7.tscn"),
-8: preload("enemy8.tscn")
+1: preload("enemies/enemy1.tscn"),
+2: preload("enemies/enemy2.tscn"),
+3: preload("enemies/enemy3.tscn"),
+4: preload("enemies/enemy4.tscn"),
+5: preload("enemies/enemy5.tscn"),
+6: preload("enemies/enemy6.tscn"),
+7: preload("enemies/enemy7.tscn"),
+8: preload("enemies/enemy8.tscn")
 }
 const MAX_ENEMIES_COUNT=4
 const HOLD_SPAWN_ENEMIES=0.5
@@ -28,7 +28,7 @@ func _ready():
 	OS.set_window_size(Vector2(256, 192)*4)
 	OS.set_window_resizable(false)
 	set_process(true)
-	get_node("lives").set_number(lives)
+	get_node("Header/LIVES").set_text(str(lives))
 
 func spawn_enemies(total):
 	var enemy_scene=enemies_scenes[current_enemy]
@@ -60,17 +60,9 @@ func _process(delta):
 	var count=enemies.get_child_count()
 	if (MAX_ENEMIES_COUNT-count)>0 and enabled_enemies:
 		time_counter+=delta
-#		print ("spawning ", MAX_ENEMIES_COUNT-count, " enemies")
-#		print ("count = ", count)
 		if time_counter>HOLD_SPAWN_ENEMIES:
 			spawn_enemies(1)
 			time_counter=0
-#	return
-#	for j in range(0, enemies.get_child_count()):
-#		var enemy=enemies.get_child(j)
-#		var pos=enemy.get_pos()
-#		var vel=enemy.velocity
-#		get_node("enemy_label"+str(j)).set_text("Enemy "+str(j) + " position: "+ str(pos) + " velocity "+ str(vel))
 
 func destroy_enemies():
 	get_tree().call_group(2, "enemies", "destroy", false)
@@ -85,13 +77,10 @@ func next_wave():
 	var ret=false
 	current_wave+=1
 	current_enemy+=1
-#	print("next_wave(): current_wave=", current_wave, "MAX_WAVES_PER_SHIP=", MAX_WAVES_PER_SHIP)
 	if current_wave >MAX_WAVES_PER_SHIP:
 		current_wave=1
 		ret = true
-#		print("next_wave(): will return true")
 	if current_enemy > TOTAL_ENEMIES_TYPES:
-#		print("next_wave():current_enemy>TOTAL ENEMY TYPES =", current_enemy, " will change to 1")
 		current_enemy=1
 		# increase velocity or number of enemies here
 	return ret
@@ -99,25 +88,20 @@ func next_wave():
 func get_max_waves_per_ship():
 	return MAX_WAVES_PER_SHIP
 
-func set_up_labels(var total):
-	var label_class=preload("res://label.tscn")
-	var world=get_node("/root/World")
-	for i in range (0, total):
-		var lab_instance=label_class.instance()
-		world.add_child(lab_instance)
-		lab_instance.set_pos(Vector2(0,13*i))
-		lab_instance.set_name("enemy_label"+str(i))
 
 func _callback_enemy_died(var p):
 	points=points+p
 	if points>hipoints:
 		hipoints=points
 	print("Points ", points)
-	get_node("Number_left").set_number(points)
-	get_node("Number_center").set_number(hipoints)
+	get_node("Header/1UP_POINTS").set_text(str(points).pad_zeros(6))
+	get_node("Header/HI_POINTS").set_text(str(hipoints).pad_zeros(6))
 
 func _callback_player_died():
 	lives-=1
-	get_node("lives").set_number(lives)
-	if lives==0:
+	if lives<=0:
+		lives=0
 		print ("GAME OVER")
+		get_node("/root/global").player1_points=points
+		get_node("/root/global").hi_points=hipoints
+	get_node("Header/LIVES").set_text(str(lives))
